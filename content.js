89ccc70dd70e75d3e4b7c5c45f7940cec95c73e0
Thomas Richards <Deskbot@users.noreply.github.com>
2024-06-22 21:33:43 +0100

    update autoplay without reloading

--- content.js
+++ content.js
@@ -1,58 +1,43 @@
 var script;
 
-function insertScript(frequency) {
+function setAutoplay(autoplay) {
+	if (script) {
+		script.parentNode.removeChild(script);
+	}
+
 	script = document.createElement("script");
 	script.id = "npafy-script";
 	script.type = "text/javascript";
-	script.innerText = [
-		"(function() {",
-		"	var ypm;",
-		"	function noAutoAdvance() {",
-		"		if (!ypm) {",
-		"			ypm = document.getElementsByTagName('yt-playlist-manager')[0];",
-		"		}",
-		"		if (ypm) {",
-		"			ypm.canAutoAdvance_ = false;",
-		"		}",
-		"	}",
-		"	noAutoAdvance();",
-		"	setInterval(noAutoAdvance, " + frequency + ");",
-		"})();"
-	].join("\n")
+	script.innerText = `
+		(function() {
+			var ypm;
+			if (!ypm) {
+				ypm = document.getElementsByTagName('yt-playlist-manager')[0];
+			}
+			if (ypm) {
+				ypm.canAutoAdvance_ = ${autoplay};
+			}
+		})();
+	`;
 
 	document.body.appendChild(script);
 }
 
-function removeScript() {
-	// var ypm = document.getElementById("npafy-script");
-	// document.body.removeChild(ypm);
-
-	if (script) {
-		document.body.removeChild(script);
-	}
+function checkAutoplay() {
+	chrome.storage.local.get(['autoplay'], function (result) {
+		setAutoplay(result.autoplay);
+	})
 }
 
-var lastAutoplay;
-setInterval(function () {
-	chrome.storage.local.get(['autoplay', 'frequency'], function (result) {
-		var frequency = result.frequency;
-		if (frequency) {
-			frequency = frequency < 250 ? 250 : frequency;
-		} else {
-			frequency = 500; // sometimes the data is lost by chrome
-		}
+chrome.storage.local.get(['frequency'], function (result) {
+	var frequency = result.frequency;
+	if (frequency) {
+		frequency = frequency < 250 ? 250 : frequency;
+	} else {
+		frequency = 500; // sometimes the data is lost by chrome
+	}
 
-		if (lastAutoplay !== result.autoplay) {
-			lastAutoplay = result.autoplay;
-
-			if (result.autoplay) {
-				console.log("remove")
-				removeScript();
-			} else {
-				console.log("insert")
-				insertScript(frequency)
-			}
-		}
-	})
-}, 500)
+	setInterval(checkAutoplay, frequency); // make sure nothing switches it back
+	checkAutoplay();
+})
 
