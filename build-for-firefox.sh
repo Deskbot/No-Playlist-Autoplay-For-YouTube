91ae52eea7368c7f0c0f179d99c70ce1f1e84bfe
Thomas Richards <Deskbot@users.noreply.github.com>
2024-12-24 16:42:05 +0000

    ensure the content of the firefox zip is not inside a folder

--- build-for-firefox.sh
+++ build-for-firefox.sh
@@ -1,5 +1,8 @@
-rm -rf output_firefox
+rm -rf output_firefox firefox.zip
 mkdir -p output_firefox
 cp -r icons scripts background.js LICENSE.txt options.html options.js output_firefox
 cp manifest_firefox.json output_firefox/manifest.json
-zip -r firefox output_firefox
+
+cd output_firefox
+zip -r ../firefox.zip *
+cd -
