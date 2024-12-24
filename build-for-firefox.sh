cb53650205896cc1f2de1b3b4fd8f6ac29b5b140
Thomas Richards <Deskbot@users.noreply.github.com>
2024-12-24 17:50:46 +0000

    ensure firefox build script gets everything

--- build-for-firefox.sh
+++ build-for-firefox.sh
@@ -1,6 +1,6 @@
 rm -rf output_firefox firefox.zip
 mkdir -p output_firefox
-cp -r icons scripts background.js LICENSE.txt options.html options.js output_firefox
+cp -r src/* LICENSE.txt output_firefox
 cp manifest_firefox.json output_firefox/manifest.json
 
 cd output_firefox
